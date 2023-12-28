4b88a0f83ee6660af8969842558eeb2cd5348388
euxn23 <euxn23@gmail.com>
2023-12-29 05:47:48 +0900

    feat: implement proxy

---
+++ proxy/src/index.ts
@@ -0,0 +1,17 @@
+export interface Env {
+  R2: R2Bucket
+}
+
+const worker: ExportedHandler<Env> = {
+  async fetch(request, env): Promise<Response> {
+    const prefix = new URL(request.url).searchParams.get('prefix')
+    if (!prefix) {
+      return new Response('{}', { status: 404, headers: { 'content-type': 'application/json;charset=UTF-8' } })
+    }
+    const imagesObjects = await env.R2.list({ prefix: prefix })
+    const imagesPaths = imagesObjects.objects.map((object) => object.key)
+    return new Response(JSON.stringify(imagesPaths))
+  },
+}
+
+export default worker
